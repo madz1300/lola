56caf21403c032503e7a5edfe8ebb23b22398296
Tyler Lutz <tlutz.computers@gmail.com>
2015-09-08 12:19:09 -0400

    Reinitializing Git Repository
    Website has basic home page.

---
+++ LOLAWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LOLAWebsite.Startup))]
+namespace LOLAWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ LOLAWebsite/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LOLAWebsite.MvcApplication" Language="C#" %>
