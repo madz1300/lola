350fef813f9a59da6f92b3dab088f91edfa965fa
Tyler Lutz <tlutz.computers@gmail.com>
2015-09-08 22:36:07 -0400

    Started Media and Contact Pages
    I have started the media and contact pages of the website. Contact form has been coded but still testing and bug fixes still
    need to be done. Need to find a image gallery and style the contact form.

--- LOLAWebsite/Controllers/HomeController.cs
+++ LOLAWebsite/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using LOLAWebsite.Models;
+using System.Text;
 
 namespace LOLAWebsite.Controllers
 {
@@ -20,16 +23,62 @@ namespace LOLAWebsite.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contact(ContactModels c)
         {
-            ViewBag.Message = "Your contact page.";
-
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    MailAddress from = new MailAddress(c.Email.ToString());
+                    StringBuilder sb = new StringBuilder();
+                    msg.To.Add("tlutz.computers@gmail.com");
+                    msg.Subject = "Contact Us";
+                    msg.IsBodyHtml = false;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new System.Net.NetworkCredential("lolaartswi@gmail.com", "landOlakes");
+                    sb.Append("First Name: " + c.FirstName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Last Name: " + c.LastName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Email: " + c.Email);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Comments: " + c.Comment);
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View("Success");
+                }
+                catch (Exception)
+                {
+                    return View("Error");
+                }
+            }
+            
             return View();
         }
 
         public ActionResult Donations()
         {
             ViewBag.Message = "Your donation page.";
+
+            return View();
+        }
+
+        public ActionResult Media()
+        {
+            ViewBag.Message = "Your media page.";
+
+            return View();
+        }
+
+        public ActionResult Success()
+        {
+            ViewBag.Message = "Successful Email";
 
             return View();
         }
